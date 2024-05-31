1a5bce65f6eb77f504f9c676ddcce5e55486a29d
Akram <iak115376@gmail.com>
2024-05-31 16:48:29 +0200

    first commit

---
+++ src/Utils/data.js
@@ -0,0 +1,84 @@
+export const skills = [
+    {
+        icon: "https://cdn-icons-png.flaticon.com/512/174/174854.png",
+        title: "HTML",
+        tag: "frontend"
+    },
+    {
+        icon: "https://cdn-icons-png.flaticon.com/512/732/732190.png",
+        title: "CSS",
+        tag: "frontend"
+    },
+    {
+        icon: "https://cdn-icons-png.flaticon.com/512/5968/5968292.png",
+        title: "JavaScript",
+        tag: "frontend"
+    },
+    {
+        icon: "https://cdn-icons-png.flaticon.com/512/5968/5968292.png",
+        title: "JavaScript",
+        tag: "backend"
+    },
+    {
+        icon: "https://cdn-icons-png.flaticon.com/512/760/760457.png",
+        title: "react",
+        tag: "frontend"
+    },
+    {
+        icon: "https://cdn-icons-png.flaticon.com/512/5968/5968381.png",
+        title: "TypeScript",
+        tag: "backend"
+    },
+    
+    {
+        icon: "https://img.icons8.com/fluency/512/node-js.png",
+        title: "Node.js",
+        tag: "backend"
+    },
+    {
+        icon: "https://img.icons8.com/color/512/nextjs.png",
+        title: "Next.js",
+        tag: "frontend"
+    },
+    {
+        icon: "https://th.bing.com/th/id/OIP.VyDhGItrznz0fphiV9yS7QHaHa?w=1024&h=1024&rs=1&pid=ImgDetMain",
+        title: "python",
+        tag: "backend"
+    },
+    {
+        icon: "https://img.icons8.com/color/512/mongodb.png",
+        title: "MongoDB",
+        tag: "backend"
+    },
+   
+    {
+        icon: "https://img.icons8.com/color/512/express-js.png",
+        title: "Express.js",
+        tag: "backend"
+    },
+    {
+        icon: "https://img.icons8.com/color/512/bootstrap.png",
+        title: "Bootstrap",
+        tag: "frontend"
+    },
+    {
+        icon: "https://img.icons8.com/ios-glyphs/512/github.png",
+        title: "Github",
+        tag: "platform"
+    },
+    {
+        icon: "https://i.ibb.co/61nck0S/download-removebg-preview.png",
+        title: "VS Code",
+        tag: "platform"
+    },
+    {
+        icon: "https://img.icons8.com/color/512/git.png",
+        title: "Git",
+        tag: "platform"
+    },
+]
+
+export const projects = [
+  
+  
+]
